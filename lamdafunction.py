823b7d2cc286e613e9328b5c9f90fe914d8ae037
Shivakumar V Hulipalled <60393577+shivakumar20@users.noreply.github.com>
2022-10-10 18:56:52 +0530

    Add files via upload

---
+++ lamdafunction.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x = lambda a,b : (a + 10) * b
+
+print(x(5,10))
